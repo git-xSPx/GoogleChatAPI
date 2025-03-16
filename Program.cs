6b0dceea6539be946b707461cad25e25c7d93aff
Serhii Pidopryhora <gml.xspx@gmail.com>
2025-03-16 18:16:42 +0200

    Some minore changes

--- Program.cs
+++ Program.cs
@@ -4,12 +4,19 @@ Env.Load();
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Додати налаштування із змінних оточення
+builder.Configuration.AddEnvironmentVariables();
+
+// Отримання порту з змінної оточення PORT та налаштування URL для Kestrel
+var port = Environment.GetEnvironmentVariable("PORT");
+if (!string.IsNullOrEmpty(port))
+{
+    builder.WebHost.UseUrls($"http://*:{port}");
+}
+
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 //.AddNewtonsoftJson();
-
-// Додати налаштування із змінних оточення
-builder.Configuration.AddEnvironmentVariables();
 
 var app = builder.Build();
 
@@ -17,7 +24,7 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // За замовчуванням HSTS має 30 днів, його можна налаштувати для продакшну.
     app.UseHsts();
 }
 
@@ -32,5 +39,4 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
-app.Run($"http://*:{port}");
+app.Run();
